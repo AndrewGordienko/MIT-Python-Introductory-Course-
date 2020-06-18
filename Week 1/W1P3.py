ee5dc537aea3dcaac8b700dd40461afae3eee1c2
AndrewGordienko <60705784+AndrewGordienko@users.noreply.github.com>
2020-06-18 18:35:10 -0400

    Update W1P3.py

--- Week 1/W1P3.py
+++ Week 1/W1P3.py
@@ -1,3 +1,8 @@
+#Assume s is a string of lower case characters.
+#Write a program that prints the longest substring of s in which the letters occur in alphabetical order.
+#For example, if s = 'azcbobobegghakl', then your program should print
+#Longest substring in alphabetical order is: beggh
+
 s = 'abcbcd'
 permanent = s[0]
 temp = permanent
