fdb680c6138badae2b02cdd2fc8db296500be447
Andrew Gordienko <60705784+AndrewGordienko@users.noreply.github.com>
2020-06-22 23:26:23 -0400

    Update W3P2.py

--- Week 3/W3P2.py
+++ Week 3/W3P2.py
@@ -1,3 +1,10 @@
+'''
+secretWord: string, the word the user is guessing
+lettersGuessed: list, what letters have been guessed so far
+returns: string, comprised of letters and underscores that represents
+what letters in secretWord have been guessed so far.
+'''
+
 def getGuessedWord(secretWord, lettersGuessed):
     endresult = ''
 
