d093c37bc7c78309f4cceab0853559f267a646d8
Andrew Gordienko <60705784+AndrewGordienko@users.noreply.github.com>
2020-06-22 23:27:37 -0400

    Update W3P3.py

--- Week 3/W3P3.py
+++ Week 3/W3P3.py
@@ -1,3 +1,9 @@
+'''
+lettersGuessed: list, what letters have been guessed so far
+returns: string, comprised of letters that represents what letters have not
+yet been guessed.
+'''
+
 def getAvailableLetters(lettersGuessed):
     string = 'abcdefghijklmnopqrstuvwxyz'
 
