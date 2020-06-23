6c5d706aa427894029e895be31394d09fcc8f583
Andrew Gordienko <60705784+AndrewGordienko@users.noreply.github.com>
2020-06-22 23:24:29 -0400

    Update W3P1.py

--- Week 3/W3P1.py
+++ Week 3/W3P1.py
@@ -1,3 +1,10 @@
+'''
+secretWord: string, the word the user is guessing
+lettersGuessed: list, what letters have been guessed so far
+returns: boolean, True if all the letters of secretWord are in lettersGuessed;
+False otherwise
+'''
+
 def isWordGuessed(secretWord, lettersGuessed):
     count = 0
 
